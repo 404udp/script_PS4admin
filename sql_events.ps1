7e1f670241225e1f81bb4c231e5a9774c67a49f9
IT UDP 404 <404udp@gmail.com>
2023-12-27 15:16:49 +0200

    Create sql_events.ps1
    у діалозі з GPT, отримуємо данні зміни стану сервисів MS-SQL серверу

---
+++ sql_events.ps1
@@ -0,0 +1,13 @@
+﻿# Задайте имя экземпляра SQL Server
+$instanceName = "MSSQL$ISQLPRO"
+
+# Получите записи из журнала событий (Application Log) о перезапуске, остановке, запуске и других событиях SQL Server
+$logEntries = Get-WinEvent -FilterHashtable @{
+    LogName = 'Application'
+    ID = 17119, 17148, 17137, 17063, 17119, 17137, 17148, 7034, 7035, 7036, 102  # Дополнительные события SQL Server
+}# | Where-Object { $_.Properties.Count -gt 0 -and $_.Properties[0].Value -eq $instanceName }
+
+# Выведите результат
+#$logEntries | Format-Table TimeCreated, Id, @{Name='InstanceName';Expression={$_.Properties[0].Value}}
+# Выведите результат
+$logEntries | Format-Table TimeCreated, Id, Message
